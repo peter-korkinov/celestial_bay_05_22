e51c65d51f1316c0be64b8993d91d904e24456dc
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-05-31 13:17:26 +0300

    configuring db

--- celestial_bay_05_22/settings.py
+++ celestial_bay_05_22/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'celestial_bay_05_22.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'celestial_bay_30_5_22',
+        'USER': 'postgres',
+        'PASSWORD': '142536mm',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
