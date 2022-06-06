408148e270bc2cdf08156db01065423f8aad5057
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2022-06-06 14:28:18 +0300

    Creating a view for registering new users

--- my_auth/views.py
+++ my_auth/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from my_auth.models import MyUser
+from my_auth.serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = MyUser.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
